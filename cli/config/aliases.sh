3167f0404bfb02891f22ebac25feb85c38ed906c
Evgheny Rastvetaev <erastvet@gmail.com>
2022-03-03 12:17:56 +0200

    feat: Add env ability to alias.sh

--- cli/config/aliases.sh
+++ cli/config/aliases.sh
@@ -1,11 +1,9 @@
-#alias composer='docker-compose -f /.devcontainer/docker-compose.yaml -p codespaces-compose run --rm --workdir="$(pwd)" composer'
-alias composer='docker-compose -f .devcontainer/docker-compose.yml -p codespaces-compose run --rm --workdir="${pwd}" composer'
-alias install='docker-compose -f .devcontainer/docker-compose.yml -p codespaces-compose run --rm --workdir="${pwd}" composer --ignore-platform-reqs create-project -n concrete5/composer application'
-# alias install="(
-# docker-compose -f .devcontainer/docker-compose.yml -p codespaces-compose run --rm composer --ignore-platform-reqs create-project -n concrete5/composer application
-# cd application
-# ./vendor/bin/concrete5 c5:install --db-server=mysql --db-username=docker --db-password=docker --db-database=docker --site='FinanceGroup' --starting-point='elemental_full' --admin-email=webhex@yandex.ru --admin-password=smart1nokia 
-# )"
-#alias composer='docker-compose -f /workspaces/concrete5/.devcontainer/docker-compose.yaml -p codespaces-compose run --rm --workdir="$(pwd)" composer --ignore-platform-reqs'
+alias install="(
+docker-compose -f .devcontainer/docker-compose.yml -p codespaces-compose run --rm composer --ignore-platform-reqs create-project -n concrete5/composer application
+cd application
+./vendor/bin/concrete5 c5:install --db-server=${DB_SERVER} --db-username=${DB_USERNAME} --db-password=${DB_PASSWORD} --db-database=${DB_DATABASE} --site=${PROJECT_NAME} --starting-point=${STARTING_POINT} --admin-email=${ADMIN_EMAIL} --admin-password=${ADMIN_PASSWORD}
+./vendor/bin/concrete5 c5:config set -g concrete.seo.url_rewriting true
+./vendor/bin/concrete5 c5:clear-cache 
+)"
 
 #alias mongo='docker-compose -f /home/vscode/workspace/vscode-devcontainer-php/.devcontainer/docker-compose.yml -p codespaces-compose run --rm --workdir="$(pwd)" mongo mongo'
